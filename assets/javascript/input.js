e7ed0a7e999dd720671b8647805b95a316d99b4a
Amor-Jansen <Amor.jansen2@gmail.com>
2022-12-06 18:03:09 +0000

    Add key events for up arrow

--- assets/javascript/input.js
+++ assets/javascript/input.js
@@ -3,14 +3,14 @@
 export class HandleInput {
     constructor(game){
         this.game = game;
-        window.addEventListener('onkeydown', e => {
+        window.addEventListener('keydown', e => {
             if (((e.key === 'ArrowUp') ||
                  (e.key === 'ArrowDown')
             )&& this.game.keys.indexOf(e.key) === -1){
                 this.game.keys.push(e.key);
             }
         });
-        window.addEventListener('onkeyup', e => {
+        window.addEventListener('keyup', e => {
             if (this.game.keys.indexOf(e.key) > -1){
                 this.game.keys.splice(this.game.keys.indexOf(e.key) > -1);
             }
