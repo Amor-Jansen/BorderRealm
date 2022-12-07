f6d0034c706d8db2a4aa6106446edd3518c0b589
Amor-Jansen <Amor.jansen2@gmail.com>
2022-12-07 09:48:39 +0000

    Add weapons key to input javascript

--- assets/javascript/input.js
+++ assets/javascript/input.js
@@ -8,6 +8,8 @@ export class HandleInput {
                  (e.key === 'ArrowDown'))
                  && this.game.keys.indexOf(e.key) === -1){
                 this.game.keys.push(e.key);
+            } else if ( e.key === ' '){
+                this.game.player.weaponTop();
             }
         });
         window.addEventListener('keyup', e => {
